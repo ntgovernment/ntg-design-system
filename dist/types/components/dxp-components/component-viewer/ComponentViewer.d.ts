7433b20044ad81a25dbb6b829e3cb39dc40fb697
peirc <Chrishan.Peiris@nt.gov.au>
2025-10-12 10:51:59 +0930

    fix storybook build and GitHub warnings

---
+++ dist/types/components/dxp-components/component-viewer/ComponentViewer.d.ts
@@ -0,0 +1,12 @@
+export type Input = Partial<{
+    componentViewerTabTitleLeft: string;
+    componentViewerTabTitleRight: string;
+    componentViewerStorybookPreview: string;
+    componentViewerCodeBlock: string;
+    componentViewerFigmaUrl: string;
+    componentViewerStorybookUrl: string;
+    componentViewerGitHubUrl: string;
+    componentViewerNewTab: string;
+}>;
+export default function main(input: Input, info?: unknown): Promise<string>;
+//# sourceMappingURL=ComponentViewer.d.ts.map
---
+++ dist/types/runtime/index.d.ts
@@ -0,0 +1,19 @@
+import { enhanceButtons } from '../components/ui-elements/button/button.runtime';
+declare function initAll(root?: ParentNode): void;
+export declare const components: {
+    button: {
+        enhanceButtons: typeof enhanceButtons;
+    };
+};
+export declare const version = "0.2.0";
+declare const _default: {
+    version: string;
+    initAll: typeof initAll;
+    components: {
+        button: {
+            enhanceButtons: typeof enhanceButtons;
+        };
+    };
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
