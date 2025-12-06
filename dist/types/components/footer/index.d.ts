850b3da259588428ead4bb3cdd61f30dda860170
Roy Galet <roy.galet2@nt.gov.au>
2025-12-07 09:21:39 +0930

    SideNavigation component added

--- dist/types/components/footer/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Footer, NTGovAULogoMono, FooterLink } from './Footer';
-export type { FooterProps, FooterSection, SocialLink } from './Footer';
-//# sourceMappingURL=index.d.ts.map
--- dist/types/components/hero-banner/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { HeroBanner } from './HeroBanner';
-export type { HeroBannerProps, HeroBannerPopularLink } from './HeroBanner';
-export { HeroBannerGraphic } from './HeroBannerGraphic';
-export type { HeroBannerGraphicProps } from './HeroBannerGraphic';
-export { HeroBannerLink } from './HeroBannerLink';
-export type { HeroBannerLinkProps } from './HeroBannerLink';
-//# sourceMappingURL=index.d.ts.map
